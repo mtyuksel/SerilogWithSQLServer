1be236aa6e5310edbedc55f628fc558f2c2df7d6
mtyuksel <yukselmtevfik@gmail.com>
2021-07-22 03:20:31 -0700

    Add project files.

---
+++ SerilogWithSQLServer.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogWithSQLServer.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                throw new NotImplementedException("This is a test error!");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(message: ex.Message, exception: ex);
+            }
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ SerilogWithSQLServer.API/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogWithSQLServer.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConfigurationRoot conf = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(conf).CreateLogger();
+
+
+            //Log.Logger = new LoggerConfiguration()
+            //    .WriteTo.MSSqlServer("Server=(local);Database=SerilogWithSqlServer;Trusted_Connection=True;", "Logs", autoCreateSqlTable: true)
+            //    .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .UseSerilog();
+                });
+    }
+}
